62e0c14b47859f6a919df0fc8f4ed3e1724b87bd
Matias Olivera <matibjolivera@gmail.com>
2020-11-28 15:13:29 -0300

    save contact taking data from API

--- app/src/main/java/com/example/contactscheduler/MainActivity.java
+++ app/src/main/java/com/example/contactscheduler/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.contactscheduler;
 
 import android.content.ContentProviderOperation;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,35 +25,40 @@ import static android.provider.ContactsContract.RawContacts;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String CONTACT_SCHEDULER_API_URL = "https://contact-scheduler-api.herokuapp.com/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        try {
-            this.createContact();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        createContact();
     }
 
-    private void createContact() throws JSONException {
-        JSONObject contact = this.getContact();
-        String name = contact.getString("name");
-        String number = contact.getString("number");
-        String email = contact.getString("email");
+    private void createContact() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, CONTACT_SCHEDULER_API_URL, response -> {
+            try {
+                JSONObject json = new JSONObject(response);
+                Contact contact = new Contact(json.getString("name"), json.getString("number"), json.getString("email"));
+                saveContact(contact);
+            } catch (JSONException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+        }, error -> Log.e("Error", error.getMessage()));
+        requestQueue.add(stringRequest);
+    }
 
+    private void saveContact(Contact contact) {
         ArrayList<ContentProviderOperation> ops = new ArrayList<>();
-
         ops.add(ContentProviderOperation.newInsert(
                 RawContacts.CONTENT_URI)
                 .withValue(RawContacts.ACCOUNT_TYPE, null)
                 .withValue(RawContacts.ACCOUNT_NAME, null)
                 .build());
-
-        this.setName(name, ops);
-        this.setNumber(number, ops);
-        this.setEmail(email, ops);
+        this.setName(contact.getName(), ops);
+        this.setNumber(contact.getNumber(), ops);
+        this.setEmail(contact.getEmail(), ops);
 
         try {
             getContentResolver().applyBatch(AUTHORITY, ops);
@@ -68,22 +79,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setNumber(String mobileNumber, ArrayList<ContentProviderOperation> ops) {
-        if (null != mobileNumber) {
+    private void setNumber(String number, ArrayList<ContentProviderOperation> ops) {
+        if (null != number) {
             ops.add(ContentProviderOperation.
                     newInsert(Data.CONTENT_URI)
                     .withValueBackReference(Data.RAW_CONTACT_ID, 0)
                     .withValue(Data.MIMETYPE,
                             Phone.CONTENT_ITEM_TYPE)
-                    .withValue(Phone.NUMBER, mobileNumber)
+                    .withValue(Phone.NUMBER, number)
                     .withValue(Phone.TYPE,
                             Phone.TYPE_MOBILE)
                     .build());
         }
     }
 
-    private void setName(String displayName, ArrayList<ContentProviderOperation> ops) {
-        if (displayName != null) {
+    private void setName(String name, ArrayList<ContentProviderOperation> ops) {
+        if (name != null) {
             ops.add(ContentProviderOperation.newInsert(
                     Data.CONTENT_URI)
                     .withValueBackReference(Data.RAW_CONTACT_ID, 0)
@@ -91,15 +102,7 @@ public class MainActivity extends AppCompatActivity {
                             StructuredName.CONTENT_ITEM_TYPE)
                     .withValue(
                             StructuredName.DISPLAY_NAME,
-                            displayName).build());
+                            name).build());
         }
-    }
-
-    private JSONObject getContact() throws JSONException {
-        JSONObject contact = new JSONObject();
-        contact.put("name", "Testing");
-        contact.put("number", "5491134160702");
-        contact.put("email", "matibjolivera@gmail.com");
-        return contact;
     }
 }
